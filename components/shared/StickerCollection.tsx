b9bbdaa4e3724404f58eabe0a60506daac258018
xSenny <ursuarsenie@gmail.com>
2024-06-15 14:44:54 +0300

    solved some error

--- components/shared/StickerCollection.tsx
+++ components/shared/StickerCollection.tsx
@@ -5,7 +5,7 @@ import StickerCard from '@/components/shared/StickerCard'
 const StickerCollection = ({stickers}: StickerCollectionProps) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {stickers.length > 0 && (
+      {stickers !== undefined && stickers.length > 0 && (
         stickers.map((s, i) => (
           <StickerCard name={s.name} uploader={s.uploader.username} id={s._id} imageUrl={s.stickerUrl}/>
         ))
